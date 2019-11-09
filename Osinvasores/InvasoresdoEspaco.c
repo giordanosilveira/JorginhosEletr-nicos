324c534a1ddcb22be34647bd05686267eadcc613
Giordano Silveira <ghs19@inf.ufpr.br>
2019-11-08 23:56:47 -0300

    Arrumando algumas funções

--- Osinvasores/InvasoresdoEspaco.c
+++ Osinvasores/InvasoresdoEspaco.c
@@ -11,18 +11,18 @@ int main () {
 
 	int tam_linhas, tam_colunas; /*resposável por pegar o tamanho da tela*/
 
-	getmaxyx (tam_linhas, tam_colunas);
-	if (tam_colunas < MINCOLUNAS || tam_linhas < MIMLINHAS) {
+	getmaxyx (stdscr, tam_linhas, tam_colunas);
+	if (tam_colunas < MINCOLUNAS || tam_linhas < MINLINHAS) {
 		printf ("O terminal deve ter no mínimo 38 linhas por 100 colunas\n");
 		endwin ();
 		return 1;
 	}
 
 	char  **corposaliens,i;
-	corposaliens = (int **)malloc(sizeof(int*)*SPRITSALIEN);
+	corposaliens = (char **)malloc(sizeof(char*)*SPRITSALIEN);
 
-	for (i = 0; i < QNTCORPOS; i++) {
-		corposaliens[i] = (int *)malloc(sizeof(int)*TAMALIEN);
+	for (i = 0; i < SPRITSALIEN; i++) {
+		corposaliens[i] = (char *)malloc(sizeof(char)*TAMALIEN);
 	}
 	initspritsalien(corposaliens); /*inicializa os corpos dos aliens*/
 
